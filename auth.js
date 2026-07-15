fafce8415c0ddc459a0da9ee39a14d514288b76a
chimuanya011 <lb4569253@gmail.com>
2026-07-15 02:12:42 +0000

    Connect register page

---
+++ auth.js
@@ -0,0 +1,57 @@
+import { auth } from "./firebase.js";
+
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+  apiKey: "AIzaSyAt7YnxQ5787wKxlui-sUniR0PP_LZXYxw",
+  authDomain: "novapay-c88fa.firebaseapp.com",
+  projectId: "novapay-c88fa",
+  storageBucket: "novapay-c88fa.firebasestorage.app",
+  messagingSenderId: "194275506659",
+  appId: "1:194275506659:web:fcaa1b1e19c559443cc8d6"
+};
+
+const fullName = document.getElementById("fullName");
+const phoneNumber = document.getElementById("phoneNumber");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+const createAccountBtn = document.getElementById("createAccountBtn");
+createAccountBtn.addEventListener("click", async () => {
+
+    if (
+        fullName.value.trim() === "" ||
+        phoneNumber.value.trim() === "" ||
+        email.value.trim() === "" ||
+        password.value === "" ||
+        confirmPassword.value === ""
+    ) {
+        alert("Please fill in all fields.");
+        return;
+    }
+
+    if (password.value !== confirmPassword.value) {
+        alert("Passwords do not match.");
+        return;
+    } 
+        try {
+
+        const userCredential = await createUserWithEmailAndPassword(
+            auth,
+            email.value.trim(),
+            password.value
+        );
+
+        alert("Account created successfully!");
+
+        window.location.href = "login.html";
+
+    } catch (error) {
+
+        alert(error.message);
+
+    }
+
+});
---
+++ firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAt7YnxQ5787wKxlui-sUniR0PP_LZXYxw",
+  authDomain: "novapay-c88fa.firebaseapp.com",
+  projectId: "novapay-c88fa",
+  storageBucket: "novapay-c88fa.firebasestorage.app",
+  messagingSenderId: "194275506659",
+  appId: "1:194275506659:web:fcaa1b1e19c559443cc8d6"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
