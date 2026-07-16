aba4ff11895a13b58d6e2fa4113838d37dba1fe3
chimuanya011 <lb4569253@gmail.com>
2026-07-16 19:49:14 +0000

    Backend foundation and funding page

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("NovaPay Backend is Running ✅");
+});app.get("/test-monnify", async (req, res) => {
+
+    try {
+
+        const auth = Buffer.from(
+            `${process.env.MONNIFY_API_KEY}:${process.env.MONNIFY_SECRET_KEY}`
+        ).toString("base64");
+
+        const response = await axios.post(
+            `${process.env.MONNIFY_BASE_URL}/api/v1/auth/login`,
+            {},
+            {
+                headers: {
+                    Authorization: `Basic ${auth}`
+                }
+            }
+        );
+
+        res.json(response.data);
+
+    } catch (error) {
+
+        res.status(500).json({
+            error: error.response?.data || error.message
+        });
+
+    }
+
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`NovaPay Server running on port ${PORT}`);
+});
