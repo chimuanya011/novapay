7061f2b079b65925c09d8decb898b401a74d272b
chimuanya011 <lb4569253@gmail.com>
2026-07-16 23:53:42 +0000

    Working backend and API routes

--- server.js
+++ server.js
@@ -1,46 +1,25 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
-require("dotenv").config();
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-
+app.use(express.json()); 
 app.get("/", (req, res) => {
-    res.send("NovaPay Backend Version 2 ✅");
-});
-;app.get("/test-monnify", async (req, res) => {
+    res.send("NovaPay Backend is Running ✅");
+}); 
+app.get("/test-monnify", async (req, res) => {
 
-    try {
+    res.json({
+        success: true,
+        message: "Monnify route is working."
+    });
 
-        const auth = Buffer.from(
-            `${process.env.MONNIFY_API_KEY}:${process.env.MONNIFY_SECRET_KEY}`
-        ).toString("base64");
-
-        const response = await axios.post(
-            `${process.env.MONNIFY_BASE_URL}/api/v1/auth/login`,
-            {},
-            {
-                headers: {
-                    Authorization: `Basic ${auth}`
-                }
-            }
-        );
-
-        res.json(response.data);
-
-    } catch (error) {
-
-        res.status(500).json({
-            error: error.response?.data || error.message
-        });
-
-    }
-
-});
-
+}); 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
